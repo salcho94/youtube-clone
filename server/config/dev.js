c883fd2a79f05bea6f92cb4e7781eb6b83c0bff6
salcho-mirincom <salcho@mirincom.co.kr>
2022-08-24 11:52:16 +0900

    test

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI:'mongodb+srv://salcho:5vlvkrhtn@youtube.atdcvbj.mongodb.net/youtube?retryWrites=true&w=majority'
+    mongoURI:'mongodb+srv://salcho:a123456789@youtube.atdcvbj.mongodb.net/youtube?retryWrites=true&w=majority'
 }
